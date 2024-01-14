0f70528d3fb6af6c77aae3af2218aff91b6cb1ff
Iban Montero <imontero@student.42urduliz.com>
2024-01-14 00:37:41 +0000

    m

--- srcs/requirements/mariadb/conf/create.sh
+++ srcs/requirements/mariadb/conf/create.sh
@@ -5,3 +5,18 @@ echo "CREATE USER IF NOT EXISTS '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD
 echo "GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%' ;" 
 echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD' ;" 
 echo "FLUSH PRIVILEGES;"
+
+#echo "CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE ;"
+# create a database with the specified name if it does not already exist.
+
+#echo "CREATE USER IF NOT EXISTS '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD' ;"
+# create a user with the specified username and password, allowing connections from any host.
+
+#echo "GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%' ;"
+# grant all privileges on the specified database to the created user.
+
+#echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD' ;"
+# alter the password for the MySQL root user when connecting from localhost.
+
+#echo "FLUSH PRIVILEGES;"
+# flush/reinit privileges, applying the changes made by the previous commands.
--- srcs/requirements/mariadb/conf/init.sh
+++ srcs/requirements/mariadb/conf/init.sh
@@ -1,18 +1,36 @@
 #!/bin/bash
 
-#Cambia el propetario del directorio de datos de MariaDB
 chown -R mysql /var/lib/mysql
 
-#Reemplazar la ubicacion del socket
 sed -i 's/^socket\s*=.*/socket=\/var\/run\/mysqld\/mysqld.sock/' /etc/mysql/mariadb.conf.d/50-server.cnf
 
-#Comenta lineas inecesarias en 50-server.cnf  para permitir conexiones externas
 sed -i "s/^bind-address\s*=.*$/#bind-address = 127.0.0.1/"  /etc/mysql/mariadb.conf.d/50-server.cnf
 sed -i "s/skip-networking/#skip-networking/g" /etc/mysql/mariadb.conf.d/50-server.cnf
 
-#Establece el puerto 3306
 sed -i "s/#\?port\s*=.*/port=3306/" /etc/mysql/mariadb.conf.d/50-server.cnf
 
 /etc/init.d/mariadb restart
 chmod +x /tmp/create.sh
 service mariadb restart
+
+#Mariadb configuration
+#chown -R mysql /var/lib/mysql
+# Change the owner recursively (in all subdirectories) of the /var/lib/mysql directory to the user 'mysql'.
+
+#sed -i 's/^socket\s*=.*/socket=\/var\/run\/mysqld\/mysqld.sock/' /etc/mysql/mariadb.conf.d/50-server.cnf
+# Use sed to edit the MariaDB server configuration file, changing the location of the MariaDB socket.
+
+#sed -i "s/^bind-address\s*=.*$/#bind-address = 127.0.0.1/"  /etc/mysql/mariadb.conf.d/50-server.cnf
+# Use sed to comment out the 'bind-address' line, allowing MariaDB to listen on all interfaces.
+
+#sed -i "s/skip-networking/#skip-networking/g" /etc/mysql/mariadb.conf.d/50-server.cnf
+# Use sed to comment out the 'skip-networking' line, allowing network connections.
+
+#sed -i "s/#\?port\s*=.*/port=3306/" /etc/mysql/mariadb.conf.d/50-server.cnf
+# Use sed to set the MariaDB port to 3306 by uncommenting and modifying the 'port' line.
+
+#/etc/init.d/mariadb restart
+# Restart the MariaDB service using the provided system init script.
+
+#service mariadb restart
+# Restart the MariaDB service using the 'service' command.
