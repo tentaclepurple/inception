224e4ebce32bb5ddba7bab400d06f508e0fc6492
Iban Montero Martinez <imontero@c4r1s5.42urduliz.com>
2024-02-19 12:59:26 +0100

    m

--- srcs/requirements/wordpress/conf/setup.sh
+++ srcs/requirements/wordpress/conf/setup.sh
@@ -23,10 +23,11 @@ wp --path=/var/www/html  config create --dbname=${MYSQL_DATABASE} --dbuser=${MYS
 # change site title. create admin user (with name, password, email). Skip email send.
 wp core install --path=/var/www/html --url=${DOMAIN_NAME} --title="INZEZION!!!" --admin_name=${WP_ADMIN_USER} --admin_user=${WP_ADMIN_USER} --admin_password=${WP_ADMIN_PASSWORD} --admin_email="imontero@student.42urduliz.com" --skip-email --allow-root 
 
-
+wp plugin install redis-cache --activate
 wp config set WP_REDIS_HOST redis --allow-root #I put --allowroot because i am on the root user on my VM
 wp config set WP_REDIS_PORT 6379 --raw --allow-root
 wp config set WP_CACHE_KEY_SALT $DOMAIN_NAME --allow-root
+wp config set WP_REDIS_DATABASE "1"
 #wp config set WP_REDIS_PASSWORD $REDIS_PASSWORD --allow-root
 wp config set WP_REDIS_CLIENT phpredis --allow-root
 wp plugin install redis-cache --activate --allow-root
